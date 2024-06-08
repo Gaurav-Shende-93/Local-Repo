6df52351e1e1c65ea2346b72c919b249b7d7d270
Gaurav-Shende-93 <shendegaurav93@gmail.com>
2024-06-08 15:44:43 +0530

    First Control Program

---
+++ sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+		int a;
+	printf("Enter the Marks\n");
+	scanf("%d",&a);
+	if(a<35)
+	{
+		printf("Fail");
+
+		}
+	else
+	{
+		printf("Pass");
+	}
+	getch;
+}
