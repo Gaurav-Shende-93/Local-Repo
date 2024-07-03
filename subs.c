042828bc6182a63c18c2b193511d2d45846203ab
Gaurav-Shende-93 <shendegaurav93@gmail.com>
2024-07-03 13:31:07 +0530

    new name

--- subs.c
+++ subs.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     int a,b;
-    printf("Enter The Number\n");
+    printf("Enter The your Number \n");
     scanf("%d%d",&a,&b);
     printf("%d",a-b);
     return 0;
